56f024495804f5b142765cc3c4a19ab2f40174b3
rifer-llx <3324134917@qq.com>
2026-06-11 17:22:20 +0800

    chore: 初始化项目配置

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // 新功能
+        "fix", // 修复 bug
+        "docs", // 文档变更
+        "style", // 代码格式（不影响代码运行的变动)
+        "perf", // 性能优化
+        "test", // 增加测试
+        "chore", // 构建过程或辅助工具的变动
+        "revert", // 回退
+        "build", // 打包
+      ],
+    ],
+    "subject-case": [0],
+  },
+};
+
+export default config;
